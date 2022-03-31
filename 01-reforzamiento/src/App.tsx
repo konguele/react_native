afff2ea1134b2796f031a6b9852b7735a34db202
konguele <juanjo.gmanzano@gmail.com>
2022-03-31 15:43:56 +0200

    Primeros pasos

---
+++ 01-reforzamiento/src/App.tsx
@@ -0,0 +1,14 @@
+import { TiposBasicos } from "./typescript/TiposBasicos";
+
+
+const App = () => {
+  return (
+    <div className="mt-2">
+      <h1>Introducción a React</h1>
+      <hr />
+      <TiposBasicos />
+    </div>
+  )
+}
+
+export default App;
